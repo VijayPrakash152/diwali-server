d744ad011d68719a514efdcb340fcfb059f66901
Manish-Auto-Parts <78971247+Manish-Auto-Parts@users.noreply.github.com>
2021-03-11 11:46:13 +0530

    Done

--- app.js
+++ app.js
@@ -13,5 +13,5 @@ setInterval(() => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`The server is running hello successfully at port ${PORT}`);
+  console.log(`The server is running  successfully at port ${PORT}`);
 });
