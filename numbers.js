03585d6e41288a8e5f57186143af22ff1e0f93ed
VijayPrakash152 <singhsidd152@gmail.com>
2021-04-15 16:31:28 +0530

    Pull

--- numbers.js
+++ numbers.js
@@ -1,6 +1,14 @@
 module.exports = () => {
-    const numbers = ['6223378731','7004373758','9088341171','7076628727','7903753242','7003160734']; // These are dummy numbers
-    let len = numbers.length - 1;                                                                    // Put the numbers here to which you 
-    let idx = Math.ceil(Math.random()*len);                                                          // want to send diwali messages!
-     return numbers[idx]; 
-}
+  const numbers = [
+    "6223378731",
+    "7004373758",
+    "9088341171",
+    "7076628727",
+    "7903753242",
+    "7003160734",
+    "2639862896",
+  ]; // These are dummy numbers
+  let len = numbers.length - 1; // Put the numbers here to which you
+  let idx = Math.ceil(Math.random() * len); // want to send diwali messages!
+  return numbers[idx];
+};
